bf72ba5a204cc5148d5de912cd6c43ba7357c235
Marcin Kosecki <marcin.kosecki@int.pl>
2021-12-17 08:44:32 +0100

    create getDay, getMounth, getYear in Event.js

--- Event.js
+++ Event.js
@@ -1,3 +1,5 @@
+// import { interfaceUser } from "./InterfaceUser.js";
+
 export class Event {
     constructor(name, day, mounth, year) {
         this.name = name;
@@ -7,7 +9,65 @@ export class Event {
 
     }
 
-   
+    getName(value) {
+        this.name = this.correctElementFromArray(value)[0];
+
+    }
+
+
+
+    cutDate(value) {
+        value = this.correctElementFromArray(value);
+        let dateToCut = []
+        let date = value.join();
+        console.log(date);
+        for (let i = 0; i < date.length; i++) {
+            if (date[i] !== '.') {
+                dateToCut[i] = date[i]
+            }
+        }
+
+        dateToCut = dateToCut.filter((el) => true)
+      
+        this.getDay(dateToCut.splice(0,2))
+        this.getMounth(dateToCut.splice(0,2))
+        this.getYear(dateToCut.splice(0,4))
+        
+    }
+
+
+    
+    getDay(value){
+        this.day = this.createNumber(value)
+
+    }
+    getMounth(value){
+        this.mounth = this.createNumber(value)
+
+    }
+    getYear(value){
+        this.year = this.createNumber(value)
+
+    }
+
+    createNumber(value){
+        let partOfDay = '';
+        for(let i = 0; i < value.length; i++){
+            partOfDay += value[i]
+        }
+        
+        return +partOfDay
+    
+    }
+
+    correctElementFromArray(value, index) {
+        let element = value.splice(length - 1, value.length);
+        return element;
+    }
+
+
+
 }
+
 
 
--- InterfaceUser.js
+++ InterfaceUser.js
@@ -6,92 +6,62 @@ import { Event } from './Event.js';
 export class InterfaceUser extends DomElements {
     constructor() {
         super();
-        this.bindToElements()
-        this.event = null;
         this.event = new Event();
+
+        this.bindToElements()
         this.pressButton();
 
     }
 
     bindToElements() {
-        this.eventNameArray = [];
-        this.evenDateArray = [];
         this.inputName = this.bindToElement(WRAPPER_INPUT_NAME_ID);
         this.inputDate = this.bindToElement(WRAPPER_INPUT_DATE_ID);
+
         this.spanParagraphName = this.bindToElement(SPAN_EVENT_NAME_ID);
         this.spanParagraphDate = this.bindToElement(SPAN_EVENT_DATE_ID)
         this.warningParagraph = this.bindToElement(WARNING_PARAGRAPH_ID)
         this.button = this.bindToElement(BUTTON_ID);
-
         this.getEventNameFromInput();
         this.getEventDateFromInput();
-
 
     }
 
     getEventNameFromInput() {
-        this.inputName.addEventListener('keyup', (e) => {
-            // @ts-ignore
-            let eventName = e.target.value;
+        this.inputName.addEventListener('keyup', this.eventName())
+    }
 
+    eventName() {
+        let eventName = [];
+        return (e) => {
+            eventName.push(e.target.value)
+            this.event.getName(eventName)
+        }
+
+    };
+
+    pressButton() {
+        this.button.addEventListener('click', () => {
+            console.log('click');
+            console.log(this.event);
         })
     }
 
     getEventDateFromInput() {
-        this.inputDate.addEventListener('keyup', (e) => {
-            // @ts-ignore
-            let eventDate = e.target.value;
-            this.evenDateArray.push(eventDate);
-            this.evenDateArray.splice(0, this.evenDateArray.length - 1);
-            this.dateEventWithoutDot(this.evenDateArray.join(' '));
-
-        })
+        this.inputDate.addEventListener('keyup', this.eventDate())
     }
 
-    dateEventWithoutDot(value) {
-        let stringDataValue = '';
-        for (let i = 0; i < value.length; i++) {
-            if(value[i] !== '.'){
-                stringDataValue += value[i]
-            }
+
+    eventDate() {
+        let eventDate = []
+        return (e) => {
+            eventDate.push(e.target.value)
+            this.event.cutDate(eventDate);
         }
-        console.log(stringDataValue);
-
     }
-
-    pressButton() {
-        this.button.addEventListener('click', () => {
-            this.fillEvent()
-            this.clearInput();
-            this.clearArrayOfEvents();
-        })
-    }
-
-    fillEvent() {
-        let arrayName = this.eventNameArray;
-        // @ts-ignore
-        this.event.name = arrayName[arrayName.length - 1];
-
-    }
-
-    clearInput() {
-        // @ts-ignore
-        this.inputDate.value = '';
-        // @ts-ignore
-        this.inputName.value = '';
-    }
-
-    clearArrayOfEvents() {
-        this.eventNameArray.length = 0;
-        console.log(this.event);
-
-    }
-
 }
 
-
 // @ts-ignore
-const interfaceUser = new InterfaceUser();
+export const interfaceUser = new InterfaceUser();
 
 
 
